913217a1f245a1f4de94988bc3c4ca9fe0b30fc1
jonathanmiddleton <jonathanmiddleton@me.com>
2025-08-29 08:40:47 -0400

    remove unnecessary while

--- tool_recursion.py
+++ tool_recursion.py
@@ -14,48 +14,47 @@ registry.register_tool(DefinitionTool.tool(), DefinitionTool.DISPATCH)
 def _completion(messages: list[Any]) -> None:
     base_url = "http://localhost:8228/v1"
     client = OpenAI(base_url=base_url, api_key="")
-    while True:
-        with client.chat.completions.stream(
-                messages=messages,
-                temperature=0.0,
-                tools=registry.tools(),
-                tool_choice="auto",
-                model="",
-                top_p=0.95,
-                frequency_penalty=1.1,
-                seed=1337,
-                max_tokens=4096,
-        ) as stream:
-            for event in stream:
-                if event.type == "tool_calls.function.arguments.done":
-                    name, args = event.name, json.loads(event.arguments)
-                    arg, result = registry.execute_tool(name, args)
-                elif event.type in ["tool_calls.function.arguments.delta"]:
-                    continue
-                elif event.type == "chunk":
-                    try:
-                        reasoning_content = event.chunk.choices[0].delta.model_extra.get("reasoning_content")
-                        if reasoning_content is not None:
-                            print(reasoning_content, end="", flush=True)
-                    except Exception as e:
-                        print(f"Error: {e}")
-                elif event.type == "content.delta":
-                    print(event.delta, end="", flush=True)
-                elif event.type == "content.done":
-                    print(event.content, end="", flush=True)
-                    return
-                else:
-                    print(f"Event: {event}")
+    with client.chat.completions.stream(
+            messages=messages,
+            temperature=0.0,
+            tools=registry.tools(),
+            tool_choice="auto",
+            model="",
+            top_p=0.95,
+            frequency_penalty=1.1,
+            seed=1337,
+            max_tokens=4096,
+    ) as stream:
+        for event in stream:
+            if event.type == "tool_calls.function.arguments.done":
+                name, args = event.name, json.loads(event.arguments)
+                arg, result = registry.execute_tool(name, args)
+            elif event.type in ["tool_calls.function.arguments.delta"]:
+                continue
+            elif event.type == "chunk":
+                try:
+                    reasoning_content = event.chunk.choices[0].delta.model_extra.get("reasoning_content")
+                    if reasoning_content is not None:
+                        print(reasoning_content, end="", flush=True)
+                except Exception as e:
+                    print(f"Error: {e}")
+            elif event.type == "content.delta":
+                print(event.delta, end="", flush=True)
+            elif event.type == "content.done":
+                print(event.content, end="", flush=True)
+                return
+            else:
+                print(f"Event: {event}")
 
-            print("\n\n")
-            print(f"argument: {arg}, result: {result}")
-            print("\n\n")
+        print("\n\n")
+        print(f"argument: {arg}, result: {result}")
+        print("\n\n")
 
-            messages.append(ChatCompletionSystemMessageParam(
-                role="system",
-                content=f"{{\"The assistant called the tool {name} with the following arguments: {arg} and returned the following result: {result}\"}}")
-            )
-            _completion(messages)
+        messages.append(ChatCompletionSystemMessageParam(
+            role="system",
+            content=f"{{\"The assistant called the tool {name} with the following arguments: {arg} and returned the following result: {result}\"}}")
+        )
+        _completion(messages)
 
 if __name__ == "__main__":
     #query = "What is a 'solidgoldmagikarp'?"
